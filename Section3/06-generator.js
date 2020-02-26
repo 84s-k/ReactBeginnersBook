1eab63945f746879489aaf39e3673af99e2faf13
84s-k <hirayama.no.uni@gmail.com>
2020-02-27 07:14:34 +0900

    P47 ジェネレーター
    戻り値からnextなどの関数が実行可能
    yieldが実行されるまでvalueに値が格納され
    実行されなくなるとvalueがundefinedとなりdoneがtrueになる。

---
+++ Section3/06-generator.js
@@ -0,0 +1,15 @@
+function* rangeGenerator(end, start = 0) {
+    let n = 0;
+
+    for (let i = start; i < end; i++) {
+        n += 1;
+        yield i;
+    }
+}
+
+const gen = rangeGenerator(3);
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
