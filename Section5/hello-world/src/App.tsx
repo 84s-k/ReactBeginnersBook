9b8a6d037df684155bb7b64114f8a26ce1e4284e
84s-k <hirayama.no.uni@gmail.com>
2020-03-04 07:39:50 +0900

    P70 〜 P73 JSXの文法について
    コメント、変展開、条件分岐、繰り返し処理、などの基本。

---
+++ Section5/hello-world/src/App.tsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    const logoOptions = {
+      alt: "logo",
+      className: "App-logo",
+      src: logo
+    };
+    const title = "こんにちは React";
+    const targets = ["world", "Kanae", "Yunika"];
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          {
+            // コメント
+          }
+          { /* コメント */ }
+          <img { ...logoOptions } />
+          { title && <p>{ title }</p>}
+          { targets.map(target => (
+            <p>Hello, { target }! </p>
+          ))}
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
